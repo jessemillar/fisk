4e1608491b73b8464e04025e209de34bc5623534
Jesse Millar <j0m01o9@m-c02tk0z9g8wl.homeoffice.wal-mart.com>
2019-08-08 16:02:16 -0500

    Minor script and README update

--- fiskify.sh
+++ fiskify.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Transcoding images"...
+echo "Transcoding images..."
 mogrify -format png $2/*.jpg
 rm $2/*.jpg
-echo "Shrinking image sizes"...
+echo "Shrinking image sizes..."
 mogrify -resize 1920 $2/*.png
-echo "Applying color palette"...
+echo "Applying color palette..."
 mogrify -dither None -remap $1 $2/*.png
 echo "Inflating image sizes..."
 mogrify -scale 5760 $2/*.png
-echo "Cropping for crisp pixels"
+echo "Cropping for crisp pixels..."
 mogrify -gravity center -crop 5760x3840+0+0 $2/*.png
 echo "Done"
